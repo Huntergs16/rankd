2094031ea16cea3346f295c9d5f92666bab7191d
Huntergs16 <samoyhun@msu.edu>
2024-10-23 07:49:10 -0500

    Initial commit

---
+++ src/lib/RankRow.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let id;
+	$: dispid = id + 1;
+	export let value = '';
+	export let src = '';
+</script>
+
+<li
+	class="flex items-center w-full rounded-md border-2 border-solid border-black h-14 duration-100 ease-in hover:h-16 cursor-pointer"
+>
+	<div class="w-6 text-xl text-right">{dispid}:</div>
+	<div>{value}</div>
+	<img {src} alt={value} />
+</li>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import RankRow from '$lib/RankRow.svelte';
+</script>
+
+<div class="w-screen h-screen bg-orange-100">
+	<header></header>
+	<div class="flex flex-col items-center justify-between">
+		<h1 class="text-7xl font-black">Rankd</h1>
+		<div>Current Item</div>
+		<ul class="w-full px-10 flex flex-col gap-3">
+			{#each { length: 10 } as _, i}
+				<RankRow id={i} />
+			{/each}
+		</ul>
+	</div>
+	<footer>© 2024 Hunter Samoy</footer>
+</div>
